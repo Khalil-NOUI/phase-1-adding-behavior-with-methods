60ec597f6850f79b15a84ccd40ab48c0dab0e7e6
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-12-28 03:41:09 +0100

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+class Cat {
+ name;
+ sex;
+
+ constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+ }
+
+ speak() {
+    return (`${this.name} says meow!`);
+ }
+ 
+}
+
+///////////////////
+
+class Dog {
+name;
+ sex;
+
+ constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    
+ }
+
+ speak = () => {
+    return(`${this.name} says woof!`);
+ };
+
+}
+
+///////////////////////
+
+class Bird {
+name;
+ sex;
+
+ constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+ }
+
+ speak() {
+    return (this.sex === "male".toLowerCase() ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`);
+ }
+
+}
